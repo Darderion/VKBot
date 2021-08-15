a10f96a08fbfb250174d8f2a23eb7b5eeb07a2ac
Darderion <Eobard1SA@gmail.com>
2021-08-16 00:57:03 +0300

    Added extension functions for PredicateAnd and PredicateOr

--- src/main/kotlin/com/gogo/steelbotrun/vkbot/core/condition/predicate/PredicateAnd.kt
+++ src/main/kotlin/com/gogo/steelbotrun/vkbot/core/condition/predicate/PredicateAnd.kt
@@ -2,6 +2,8 @@ package com.gogo.steelbotrun.vkbot.core.condition.predicate
 
 import com.gogo.steelbotrun.vkbot.core.condition.Condition
 
+infix fun Condition.and(other: Condition) = PredicateAnd(this, other)
+
 class PredicateAnd(vararg conditions: Condition): PredicateBinary(*conditions) {
 	override fun getPredicateTree(vararg conditions: Condition) = Pair(
 		PredicateAnd(*conditions.copyOfRange(0, (conditions.count() + 1) / 2)),
--- src/test/kotlin/com/gogo/steelbotrun/vkbot/core/condition/ConditionsTests.kt
+++ src/test/kotlin/com/gogo/steelbotrun/vkbot/core/condition/ConditionsTests.kt
@@ -1,10 +1,13 @@
 package com.gogo.steelbotrun.vkbot.core.condition
 
+import com.gogo.steelbotrun.vkbot.core.condition.predicate.and
+import com.gogo.steelbotrun.vkbot.core.condition.predicate.or
 import com.gogo.steelbotrun.vkbot.game.character.stats.Stats
 import com.gogo.steelbotrun.vkbot.game.condition.ConditionStat
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.comparables.shouldBeEqualComparingTo
+import io.kotest.matchers.shouldBe
 
 class ConditionsTests: StringSpec({
 	"ConditionStat should compare stats" {
@@ -22,5 +25,23 @@ class ConditionsTests: StringSpec({
 		shouldThrowAny {
 			condition.interpret(Stats("Strength" to 3.0), 1, 2) shouldBeEqualComparingTo false
 		}
+	}
+	"(A and B) should be true if A == true and B == true" {
+		val a = ConditionStat("strength", ComparisonType.Greater, 3.0)
+		val b = ConditionStat("strength", ComparisonType.Less, 5.0)
+
+		(a and b).interpret(Stats("strength" to 4.0)) shouldBe true
+		(a and b).interpret(Stats("strength" to 5.0)) shouldBe false
+		(a and b).interpret(Stats("strength" to 2.0)) shouldBe false
+	}
+
+	"(A or B) should be true if A == true or B == true" {
+		val a = ConditionStat("strength", ComparisonType.Less, 3.0)
+		val b = ConditionStat("strength", ComparisonType.Greater, 5.0)
+
+		(a or b).interpret(Stats("strength" to 4.0)) shouldBe false
+		(a or b).interpret(Stats("strength" to 5.0)) shouldBe false
+		(a or b).interpret(Stats("strength" to 2.0)) shouldBe true
+		(a or b).interpret(Stats("strength" to 7.0)) shouldBe true
 	}
 })
